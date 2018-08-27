7d3d540d347bade81c41598380490ff6b3e81657
Jakub Bialy <bialyj1@gmail.com>
2018-08-27 23:53:31 +0100

    Project set up

---
+++ src/main/java/own/jb/onlinevotingplatform/OnlineVotingPlatformApplication.java
@@ -0,0 +1,12 @@
+package own.jb.onlinevotingplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineVotingPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineVotingPlatformApplication.class, args);
+    }
+}
